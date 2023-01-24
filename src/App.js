e28ba63a579a6189d3046d6cb4898356368e50a1
Todd Peltzer <toddpeltzer14@gmail.com>
2023-01-24 07:33:36 -0700

    cleaned up default react files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Main from './components/Main';
